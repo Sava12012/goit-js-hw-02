552899b75735a4e15a0631008004bb97204c1737
Dmytro Savych <sava.savych@gmail.com>
2022-08-27 20:28:05 +0300

    Виконано 1 завданян 2 модулю

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+// Work 1
+
+function checkAge(age) {
+  if (age>=18) { // Change this line
+    return "You are an adult";
+  }
+  return "You are a minor";
+}
+
+checkAge(45);
+checkAge(10);
+checkAge(12);
+checkAge(51);
