178f9c33649842c54aaeab3d2511627d90ae04b3
Dmytro Savych <sava.savych@gmail.com>
2022-08-27 20:49:54 +0300

    виконано 5 завдання модулю 2

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,48 @@
-// Work 1
+// =========== Work 1
 
-function checkAge(age) {
-  if (age>=18) { // Change this line
-    return "You are an adult";
-  }
-  return "You are a minor";
-}
+// function checkAge(age) {
+//   if (age>=18) { // Change this line
+//     return "You are an adult";
+//   }
+//   return "You are a minor";
+// }
 
-checkAge(45);
-checkAge(10);
-checkAge(12);
-checkAge(51);
+// checkAge(45);
+// checkAge(10);
+// checkAge(12);
+// checkAge(51);
+
+
+// =========== Work 2
+
+// function checkPassword(password) {
+//     const ADMIN_PASSWORD = "jqueryismyjam";
+//     if (password === ADMIN_PASSWORD) {
+//         return "Welcome!";
+//     }
+    
+//     return "Access denied, wrong password!"
+//     console.log(checkPassword);
+// }
+
+// =========== Work 3
+
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+  
+
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   } if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   } 
+//     return "The order is accepted, our manager will contact you";   
+// }
+
+// =========== Work 4
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// console.log(fruits); 
+
+// =========== Work 5
