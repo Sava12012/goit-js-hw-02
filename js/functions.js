79474ebcdee26a85b444173f82321732c0026549
Dmytro Savych <sava.savych@gmail.com>
2022-08-29 23:32:14 +0300

    15

--- js/functions.js
+++ js/functions.js
@@ -126,3 +126,12 @@
 // const lastThreeEls = fruits.slice(-3);
 
 // // =========== Work 15
+
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// const allClients = oldClients.concat(newClients);
+
+// console.log(allClients);
+
+// // =========== Work 16
