ad9d098399f3912cac3297297f609a4d1af405b7
Dmytro Savych <sava.savych@gmail.com>
2022-08-30 18:44:22 +0300

    20

--- js/functions.js
+++ js/functions.js
@@ -167,3 +167,16 @@
 
 // // =========== Work 20
 
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+//   for (let i = 0; i < order.length; i += 1 ) {
+//     total = total + order[i];
+    
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+
+ // =========== Work 21
