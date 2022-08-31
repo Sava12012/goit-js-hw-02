3227fd68c61c76885190c6c55b2d332ad4c0da83
Dmytro Savych <sava.savych@gmail.com>
2022-08-31 15:02:53 +0300

    new notes

---
+++ notebook/notes.js
@@ -0,0 +1,46 @@
+//                         Модуль 2 - Масиви Функції
+// =========== Перевірка на масив: 
+let friends = ['vova', 'bob', 'anna', 'dima', 'nata']
+console.log(friends)
+
+// let nums = [1, 2, 3, 4, 5];
+// console.log(nums);
+// console.log(Array.isArray(nums));
+
+
+// =========== Пошук по масиву: [].indexOf()
+
+// let friends = ['vova', 'bob', 'anna', 'dima']
+// let result = friends.indexOf('dima');
+// console.log(result);
+
+
+// =========== Додає в кінець 1 або декілька елементів: [].push();
+
+// friends.push("sveta", 200, "lena");
+// console.log(friends);
+
+// =========== Видалити з масиву: [].splice()
+
+// friends.splice(1);
+// friends.splice(1, 1);
+// friends.splice(1, 1, "pizza");
+// console.log(friends)
+
+// =========== Видалити з масиву: [].slice()
+
+// 1 Зробити копію масиву:
+// const nums = [10, 20, 30, 40];
+// console.log(nums);
+// const nums_2 = nums.slice();
+// console.log(nums_2)
+
+// 2 Ріжем самив за допомогою.slice
+
+// const nums = [10, 20, 30, 40];
+// console.log(nums);
+
+// let nums_2 = nums.slice(2);
+// console.log(nums_2);
+
+
