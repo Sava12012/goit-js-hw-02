7d89c630d488cbf090313fa45855e1a01f04d3da
Dmytro Savych <sava.savych@gmail.com>
2022-09-04 20:08:03 +0300

    19

--- js/functions.js
+++ js/functions.js
@@ -323,3 +323,23 @@
 
 
 //  =========== Work 19
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+
+
+// let prodValue = [];
+//   for (let product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       prodValue.push(product[propName]);
+//     }
+//   }
+//   return prodValue;
+
+// }
