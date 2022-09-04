a3bad088bb7da45f542ad590d82afcd6def7da65
Dmytro Savych <sava.savych@gmail.com>
2022-09-04 22:58:00 +0300

    25

--- js/functions.js
+++ js/functions.js
@@ -382,4 +382,22 @@
 // const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
 
 
-//  =========== Work 24
+//  =========== Work 25
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+
+
+// const {today: {low: lowToday, high: highToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg"}, tomorrow: {low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'}} = forecast;
+
+
+//  =========== Work 26
