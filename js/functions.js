ae529b0eb048efc3aef03b2b84f30fa028bf645c
Dmytro Savych <sava.savych@gmail.com>
2022-09-12 23:31:19 +0300

    24

--- js/functions.js
+++ js/functions.js
@@ -497,14 +497,94 @@
 
 //======= Work 3
 
-// // Колбек-функція
-// function greet(name) {
-//   console.log(`Ласкаво просимо ${name}.`);
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   numbers.foreach(pushArray(item, value)) {
+//     if (item > value) {
+//     filteredNumbers.push(item);
+//     }
+//   }   
+// return filteredNumbers;
 // }
 
-// // Функція вищого порядку
-// function registerGuest(name, callback) {
-//   console.log(`Реєструємо гостя ${name}.`);
-//   callback(name);
-// }
-// registerGuest("Манго", greet);
+//======= Work 24
+
+[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]
+
+const getUsersWithFriend = (users, friendName) => {
+   return users.filter(
+    user => user.friends.includes(friendName))
+};
+
+console.log(friendName(Goldie Gentry));
--- notebook/notes.js
+++ notebook/notes.js
@@ -185,23 +185,69 @@
 // оголосити безпосередньо на момент виклику функції, в яку передаємо колбек.Така функція 
 // буде доступна тільки у якості значення параметра і більше ніде в коді.
 
-function registerGuest(name, callback) {
-  console.log(`Реєструємо гостя ${name}.`);
-  callback(name);
-}
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
 
-// Передаємо інлайн функцію greet у якості колбека
-registerGuest("Манго", function greet(name) {
-  console.log(`Ласкаво просимо ${name}.`);
-});
+// // Передаємо інлайн функцію greet у якості колбека
+// registerGuest("Манго", function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// });
 
-// Передаємо інлайн функцію notify у якості колбека
-registerGuest("Полі", function notify(name) {
-  console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
-});
+// // Передаємо інлайн функцію notify у якості колбека
+// registerGuest("Полі", function notify(name) {
+//   console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+// });
 
+// Декілька колбеків
 
+// function processCall(recipient) {
+//   // Імітуємо доступність абонента випадковим числом
+//   const isRecipientAvailable = Math.random() > 0.5;
 
+//   if (!isRecipientAvailable) {
+//     console.log(`Абонент ${recipient} недоступний, залиште повідомлення.`);
+//     // Логіка активації автовідповідача
+//   } else {
+//     console.log(`З'єднуємо з ${recipient}, очікуйте...`);
+//     // Логіка прийняття дзвінка
+//   }
+// }
+// processCall("Манго");
+
+// Виконаємо рефакторинг функції таким чином, щоб вона приймала два колбеки 
+// onAvailable і onNotAvailable, і викликала їх за умовою.
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Імітуємо доступність абонента випадковим числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`З'єднуємо з ${name}, очікуйте...`);
+//   // Логіка прийняття дзвінка
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+//   // Логіка активації автовідповідача
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+//   // Логіка запису голограми
+// }
+
+// processCall("Манго", takeCall, activateAnsweringMachine);
+// processCall("Полі", takeCall, leaveHoloMessage);
 
                                 // Модуль 4 - Перебираючі методи масиву
 
